31d8ef8403a28aed51fd6f5f2b945ef25ae24b43
jenny <jennyffer_delgado23201@elpoli.edu.co>
2023-03-01 17:17:02 -0500

    Metodo de suma

--- src/co/edu/poli/ces3/Estudiante.java
+++ src/co/edu/poli/ces3/Estudiante.java
@@ -4,4 +4,7 @@ public class Estudiante {
     public static void main(String[] args) {
         System.out.println("Hola poli...");
     }
+    public static int sumar(int a, int b){
+        return a + b;
+    }
 }
